f7114202b342a93043fae44c6ff7b5067b8bf5e2
Paul Solt <PaulSolt@gmail.com>
2018-01-29 15:53:46 -0500

    Added sample project for Auto Layout and UIScrollView

---
+++ ScrollViewAutoLayout/ScrollViewAutoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollViewAutoLayout
+//
+//  Created by Paul Solt on 1/29/18.
+//  Copyright © 2018 Paul Solt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
